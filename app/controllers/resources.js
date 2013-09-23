fce31ef3f878bd388f56ecfc1778b22a65a8af78
CHH <chh@chh.tw>
2013-09-24 02:41:18 +0800

    add semicolons to the code...

--- app/controllers/resources.js
+++ app/controllers/resources.js
@@ -21,10 +21,10 @@ var mongoose = require('mongoose')
 exports.index = function(req, res, next) {
 
   Resource.list({}, function(err, data) {
-    res.send(data)
-  })
+    res.send(data);
+  });
 
-}
+};
 
 /**
  * Show
@@ -34,7 +34,7 @@ exports.show = function (req, res, next) {
   Resource.getResourceById(req.params.id, function (err, resource) {
     res.send(resource);
   });
-}
+};
 
 /**
  * Create
@@ -43,44 +43,45 @@ exports.show = function (req, res, next) {
 exports.create = function(req, res, next) {
 
   var name = sanitize(req.body.name).trim()
-    , name = sanitize(name).xss()
+    , name = sanitize(name).xss();
 
   var description = sanitize(req.body.description).trim()
-    , description = sanitize(description).xss()
+    , description = sanitize(description).xss();
 
   var img_url = sanitize(req.body.img_url).trim()
-    , img_url = sanitize(img_url).xss()
+    , img_url = sanitize(img_url).xss();
 
   var url = sanitize(req.body.url).trim()
-    , url = sanitize(url).xss()
+    , url = sanitize(url).xss();
 
   var type = sanitize(req.body.type).trim()
-    , type = sanitize(type).xss()
+    , type = sanitize(type).xss();
 
-  var resource = new Resource()
+  var resource = new Resource();
 
   resource.newAndSave(name, description, img_url, url, type, function(err, resource) {
 
     if (err) {
-      console.error(err)
-      if (err.code == 11000)
-        res.send(403, 'Duplicate resource name.')
-      return next(err)
+      console.error(err);
+      if (err.code == 11000) {
+        res.send(403, 'Duplicate resource name.');
+      }
+      return next(err);
     }
     else if (!resource) {
-      var err_msg = "Resource not saved"
-      res.send(403, err_msg)
-      return next(new Error(err_msg))
+      var err_msg = "Resource not saved";
+      res.send(403, err_msg);
+      return next(new Error(err_msg));
     }
 
     Type.getTypeById(req.body.type, function(err, type) {
-      type.resources_count += 1
-      type.save()
+      type.resources_count += 1;
+      type.save();
 
       Resource.getResourceById(resource._id, function(err, resource) {
-        res.send(resource)
-      })
-    })
+        res.send(resource);
+      });
+    });
 
-  })
-}
+  });
+};
